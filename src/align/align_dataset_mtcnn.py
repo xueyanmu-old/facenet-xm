e7e5b8226f6860f4411b98e4983d4d3582c8926d
XueyanMu <xm14@rice.edu>
2021-06-21 16:26:31 -0300

    minor changes to allow imageio

--- src/align/align_dataset_mtcnn.py
+++ src/align/align_dataset_mtcnn.py
@@ -36,6 +36,11 @@ import align.detect_face
 import random
 from time import sleep
 
+import matplotlib.pyplot as plt
+from PIL import Image
+import imageio
+
+
 def main(args):
     sleep(random.random())
     output_dir = os.path.expanduser(args.output_dir)
@@ -80,7 +85,7 @@ def main(args):
                 print(image_path)
                 if not os.path.exists(output_filename):
                     try:
-                        img = misc.imread(image_path)
+                        img = plt.imread(image_path)
                     except (IOError, ValueError, IndexError) as e:
                         errorMessage = '{}: {}'.format(image_path, e)
                         print(errorMessage)
@@ -121,14 +126,15 @@ def main(args):
                                 bb[2] = np.minimum(det[2]+args.margin/2, img_size[1])
                                 bb[3] = np.minimum(det[3]+args.margin/2, img_size[0])
                                 cropped = img[bb[1]:bb[3],bb[0]:bb[2],:]
-                                scaled = misc.imresize(cropped, (args.image_size, args.image_size), interp='bilinear')
+                                scaled = np.array(Image.fromarray(cropped).resize((args.image_size, args.image_size),
+                                                                                  resample=Image.BILINEAR))
                                 nrof_successfully_aligned += 1
                                 filename_base, file_extension = os.path.splitext(output_filename)
                                 if args.detect_multiple_faces:
                                     output_filename_n = "{}_{}{}".format(filename_base, i, file_extension)
                                 else:
                                     output_filename_n = "{}{}".format(filename_base, file_extension)
-                                misc.imsave(output_filename_n, scaled)
+                                imageio.imwrite(output_filename_n, scaled)
                                 text_file.write('%s %d %d %d %d\n' % (output_filename_n, bb[0], bb[1], bb[2], bb[3]))
                         else:
                             print('Unable to align "%s"' % image_path)
